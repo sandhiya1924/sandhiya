ebc464112cd70261f1d706ee356fe0831880549a
sandhiya1924 <santhiya22td0717@svcet.ac.in>
2025-01-31 16:31:03 +0530

    added

---
+++ app1.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+    def get(self):
+        return {"message": "Hello, World!"}
+
+# This line should be outside of the class definition
+api.add_resource(HelloWorld, '/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
